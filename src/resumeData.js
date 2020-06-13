c9a8b3e9ce18ccfbef90caa5086c29d16454e70a
BHUSHAN PATEL <bhushanketanbhai.patel@calbaptist.edu>
2020-06-13 14:26:31 -0700

    make it better

---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+    "imagebaseurl":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.crestinfotech.com%2Four-portfolio%2F&psig=AOvVaw1HxGcbn-PHvmZEzdMyyHp5&ust=1591661624465000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiQx-D38OkCFQAAAAAdAAAAABAD",
+    "name": "Bhushan Patel",
+    "role": "Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/bhushan-patel/",
+    "gmailId":"patelbhushan431997@gmail.com",
+    "skypeid": "patelbhushan431997@gmail.com",
+    "phoneNo": "+1 (951)-772-6450",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, simply play cricket in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/bhushan-patel/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/bhushan43199",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/montu_431997",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am recently graduated form California Baptist University with Master degree in Software Engineering. I am a self taught Web Developer, currently diving deeper into full stack developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/bhushan43199",
+    "education":[
+      
+      {
+        "UniversityName":"California Baptist University",
+        "specialization":"Masters in Software Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2020",
+      },
+      {
+        "UniversityName":"Gujarat Technological University",
+        "specialization":"Bachelors in Computer Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2018",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Webpoint Infotech",
+        "specialization":"Web Developer Intern",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2017 to 2018",
+        "Achievements":[
+          "Involved in the design and development of the web application using JavaScript, HTML, CSS etc.",
+          "Accelerated skills in development, requirement analysis, documentation, planning, testing and debugging",
+           "Part of the development team of the web-app and worked on several software projects.",
+           "Provide clear, detailed descriptions of web site specifications such as product features, activities, software, communication protocols, programming languages, and operating systems software and hardware",
+           "Developed and maintained a website for the company using Visual Basic, HTML, CSS, and Bootstrap. • Prepared technical support for the company.",
+          
+        ]
+      }
+    ],
+    /* "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ], */
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    /* "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ] */
+  }
+  
+  export default resumeData
